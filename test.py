49b7f080038bd29fa5c4ff4a03a8c97bd01cd68d
pramod <pramodgond0075@gmail.com>
2025-09-27 14:46:29 +0530

    Deployment ECS Fixed

--- test.py
+++ test.py
@@ -153,7 +153,7 @@ def test_document_ingestion_and_rag():
         
         
 # 23 aug - day 13 - part 1
-# test file
+# test file to test
         
 if __name__ == "__main__":
     test_document_ingestion_and_rag()
